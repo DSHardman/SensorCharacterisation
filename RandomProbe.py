f80710202fc8a9adde81de1fec665b638098bca4
David <dshardman@outlook.com>
2021-07-06 17:01:19 +0100

    ur5 controls for vhb table sensors

--- RandomProbe.py
+++ RandomProbe.py
@@ -6,13 +6,18 @@ from datetime import datetime
 
 #upper_bound = 30
 #lower_bound = 15
-upper_bound = 45
-lower_bound = 15
 
-def newspot(x, y, time_start):
-    #urnie.movel([-0.0489+x/1000, -0.321+y/1000, 0.021, 1.88051, -2.51452, 0.0161891], acc=0.02, vel=0.02)
-    #urnie.movel([-0.0721 + x / 1000, -0.316 + y / 1000, 0.0260109, 1.87748, -2.50178, 0.0212946], acc=0.02, vel=0.02)
-    urnie.movel([-0.0580 + x / 1000, -0.3546 + y / 1000, 0.0255, 1.8981, -2.5004, 0.00901938], acc=0.02, vel=0.02)
+upper_bound = 35
+lower_bound = 10
+
+#upper_bound = 45
+#lower_bound = 15
+
+position = [-0.126935, -0.329702, 0.00108133, 1.89085, -2.50308, 0.0134363]  #left sensor
+#position = [-0.0187031, -0.329943, 0.00108133, 1.89087, -2.50319, 0.0132253]  #right sensor
+
+def newspot(x, y, time_start, position):
+    urnie.movel([position[0] + x / 1000, position[1] + y / 1000, position[2] +0.0235, position[3], position[4], position[5]], acc=0.02, vel=0.02)
     urnie.translatel_rel([0, 0, -0.0235, 0, 0, 0], acc=0.02, vel=0.02)
     time.sleep(2)
     urnie.translatel_rel([0, 0, 0.0235, 0, 0, 0], acc=0.02, vel=0.02)
@@ -27,7 +32,7 @@ for i in range(50):
     time_start = time.time()
     x = random.random()*(upper_bound-lower_bound) + lower_bound
     y = random.random()*(upper_bound-lower_bound) + lower_bound
-    newspot(x, y, time_start)
+    newspot(x, y, time_start, position)
 
     #now = datetime.now()
     #dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
--- RandomProbe/PlotErrorLocations.m
+++ RandomProbe/PlotErrorLocations.m
@@ -17,7 +17,7 @@ subplot(2,3,1);
 contourf(xx,yy,error_interp);
 title('A errors');
 caxis([0 20]);
-text(15,15,string(mean(Aerrors)),'Fontsize',15);
+%text(15,15,string(mean(Aerrors)),'Fontsize',15, 'Color', 'w');
 
 Berrors = zeros(length(Bcleanrelfil),1);
 for i = 1:length(Bcleanrelfil)
@@ -36,7 +36,7 @@ subplot(2,3,4);
 contourf(xx,yy,error_interp)
 title('B errors');
 caxis([0 20]);
-text(15,15,string(mean(Berrors)),'Fontsize',15);
+%text(15,15,string(mean(Berrors)),'Fontsize',15);
 
 %% Lost sensors: 4 & 5 broken & unknown
 
@@ -56,7 +56,7 @@ subplot(2,3,2);
 contourf(xx,yy,error_interp);
 title('A errors: unknown damage');
 caxis([0 20]);
-text(15,15,string(mean(Aerrors)),'Fontsize',15);
+%text(15,15,string(mean(Aerrors)),'Fontsize',15);
 
 Berrors = zeros(length(Bcleanrelfil),1);
 for i = 1:length(Bcleanrelfil)
@@ -75,7 +75,7 @@ subplot(2,3,5);
 contourf(xx,yy,error_interp)
 title('B errors: unknown damage');
 caxis([0 20]);
-text(15,15,string(mean(Berrors)),'Fontsize',15);
+%text(15,15,string(mean(Berrors)),'Fontsize',15);
 
 %% Lost sensors: 4 & 5 broken & known
 
@@ -96,7 +96,7 @@ subplot(2,3,3);
 contourf(xx,yy,error_interp);
 title('A errors: 4 & 5 Break');
 caxis([0 20]);
-text(15,15,string(mean(Aerrors)),'Fontsize',15);
+%text(15,15,string(mean(Aerrors)),'Fontsize',15);
 
 Berrors = zeros(length(Bcleanrelfil),1);
 for i = 1:length(Bcleanrelfil)
@@ -116,4 +116,4 @@ subplot(2,3,6);
 contourf(xx,yy,error_interp)
 title('B errors: 4 & 5 Break');
 caxis([0 20]);
-text(15,15,string(mean(Berrors)),'Fontsize',15);
+%text(15,15,string(mean(Berrors)),'Fontsize',15);
